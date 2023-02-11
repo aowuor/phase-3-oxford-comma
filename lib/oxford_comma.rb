c7a300d675e3d2ec41fea6ac41fae6ab22c70cfe
aowuor <angelaowuor@gmail.com>
2023-02-12 02:53:42 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+# require 'pry'
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array.each do |element|
+            if(element == array[-1])
+                array[-1] = "and " << element
+            end 
+        end
+    array.join(", ")
+    end
+ 
 end
+puts oxford_comma(["a", "b", "c"])
+# binding.pry
